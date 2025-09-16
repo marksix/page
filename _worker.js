522f89feb775e6a94fb9a27e9363070e3f5cf6ae
marksix <82834747+marksix@users.noreply.github.com>
2025-09-16 15:24:47 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,9 +1,9 @@
 export default {
   async fetch(request, env) {
-    // 从目录里找静态文件
+    // 从目录返回静态资源
     let res = await env.ASSETS.fetch(request);
 
-    // 如果 404 且请求的是 HTML，就回退到 index.html（适合单页面应用）
+    // 如果 404 且浏览器要 HTML，就回退到 index.html（适合单页面应用）
     if (res.status === 404 && request.headers.get("accept")?.includes("text/html")) {
       return env.ASSETS.fetch(new Request(new URL("/index.html", request.url), request));
     }
